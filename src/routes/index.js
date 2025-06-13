745f245e3eddcef6d03d56668b4fb1b23ff5a507
Glfrancodev <gabrielandreslopezfrancodev@gmail.com>
2025-06-13 00:33:24 -0400

    Ping al Front para PWA

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const usuarioRoutes = require('./usuario.routes');
-const authRoutes = require('./auth.routes');
-const proyectoRoutes = require('./proyecto.routes');
+const usuarioRoutes    = require('./usuario.routes');
+const authRoutes       = require('./auth.routes');
+const proyectoRoutes   = require('./proyecto.routes');
 const invitacionRoutes = require('./invitacion.routes');
-const archivoRoutes = require('./archivo.routes');
+const archivoRoutes    = require('./archivo.routes');
+
+// ✅ Ruta de verificación (para PWA / offline detection)
+router.get('/ping', (_, res) => res.send('pong'));
 
 router.use('/api/usuarios', usuarioRoutes);
 router.use('/api/auth', authRoutes);
